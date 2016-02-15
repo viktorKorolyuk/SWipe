88eca255861fb17357396386f1e20eee55356ed0
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-02-15 13:29:27 -0500

    dd

--- code.js
+++ code.js
@@ -3,8 +3,7 @@ var scar = document.getElementById("scar");
 var main = document.getElementById("mainBLOCK");
 var bb = document.getElementById("bb");
 var ba = document.getElementById("ba");
-var upKEY = null;
-var dwnKEY = null;
+var KEYdir = null;
 var time;
 var seconds = 0;
 var randAns = [1, 2];
@@ -33,29 +32,28 @@ function randomChoice() {
 }
 window.onkeydown = function (e) {
     if (e.keyCode == "38") {
-        upKEY = true;
-        dwnKEY = false;
+        KEYdir = 1;
     } else if (e.keyCode == "40") {
-        upKEY = false;
-        dwnKEY = true;
+        KEYdir = 0;
+
     }
 };
 function gameLoop() {
-    if (dwnKEY === true && main.style.backgroundColor === "rgb(186, 218, 85)" && bb.style.backgroundColor === "rgb(186, 218, 85)") { //down key with green color and green color
+    if (KEYdir === 0 && main.style.backgroundColor === "rgb(186, 218, 85)" && bb.style.backgroundColor === "rgb(186, 218, 85)") { //down key with green color and green color
         win();
-    } else if (dwnKEY === true && main.style.backgroundColor === "rgb(186, 218, 85)" && bb.style.backgroundColor === "rgb(255, 102, 102)") { //down key with green color and red color
+    } else if (KEYdir === 0 && main.style.backgroundColor === "rgb(186, 218, 85)" && bb.style.backgroundColor === "rgb(255, 102, 102)") { //down key with green color and red color
         loose();
-    } else if (dwnKEY === true && main.style.backgroundColor === "rgb(255, 102, 102)" && bb.style.backgroundColor === "rgb(255, 102, 102)") { //down key with green color and red color
+    } else if (KEYdir === 0 && main.style.backgroundColor === "rgb(255, 102, 102)" && bb.style.backgroundColor === "rgb(255, 102, 102)") { //down key with green color and red color
         win();
-    } else if (dwnKEY === true && main.style.backgroundColor === "rgb(255, 102, 102)" && bb.style.backgroundColor === "rgb(186, 218, 85)") { //down key with green color and red color
+    } else if (KEYdir === 0 && main.style.backgroundColor === "rgb(255, 102, 102)" && bb.style.backgroundColor === "rgb(186, 218, 85)") { //down key with green color and red color
         loose();
-    }else if (upKEY === true && main.style.backgroundColor === "rgb(255, 102, 102)" && ba.style.backgroundColor === "rgb(255, 102, 102)") {
+    }else if (KEYdir = 1; && main.style.backgroundColor === "rgb(255, 102, 102)" && ba.style.backgroundColor === "rgb(255, 102, 102)") {
         win();
-    } else if (upKEY === true && main.style.backgroundColor === "rgb(255, 102, 102)" && ba.style.backgroundColor === "rgb(186, 218, 85)") {
+    } else if (KEYdir = 1; && main.style.backgroundColor === "rgb(255, 102, 102)" && ba.style.backgroundColor === "rgb(186, 218, 85)") {
         loose();
-    }else if (upKEY === true && main.style.backgroundColor === "rgb(255, 102, 102)" && ba.style.backgroundColor === "rgb(186, 218, 85)") {
+    }else if (KEYdir = 1; && main.style.backgroundColor === "rgb(255, 102, 102)" && ba.style.backgroundColor === "rgb(186, 218, 85)") {
         loose();
-    } else if (upKEY === true && main.style.backgroundColor === "rgb(186, 218, 85)" && ba.style.backgroundColor === "rgb(186, 218, 85)") {
+    } else if (KEYdir = 1; && main.style.backgroundColor === "rgb(186, 218, 85)" && ba.style.backgroundColor === "rgb(186, 218, 85)") {
         win();
     }
 }
