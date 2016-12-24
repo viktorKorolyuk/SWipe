fa4752aba7f43ab76f65af6cfdc820b6e9b8ded8
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-12-23 23:56:18 -0500

    Added start button.

--- code.js
+++ code.js
@@ -9,16 +9,9 @@ var score = 0;
 var colors = ["rgb(255, 237, 102)", "rgb(186, 218, 85)", "rgb(75, 204, 220)"];
 var ranTIMES = [2500, 1000, 2000, 5000, 3000, 1000, 5000];
 var timewait = 0.01;
-var timer = 30;
+var timerValue = 30;
 
-top_b.onclick = function () {
-    check(38);
-};
-bottom.onclick = function () {
-    check(40);
-};
-
-var randomize = function () {
+var randomize = function() {
     var wheight = colors[Math.floor(Math.random() * colors.length)];
     var wheight2 = colors[Math.floor(Math.random() * colors.length)];
     var opt = [];
@@ -34,60 +27,72 @@ var randomize = function () {
     subject.style.backgroundColor = opt[Math.floor(Math.random() * opt.length)];
 };
 
-var check = function (key) {
+var check = function(key) {
     if (colorsc[key] === subject.style.backgroundColor) {
         score++;
         randomize();
-
-        timer = timer * 1.15 + 1; //add a bit to the timer
+        timerValue = timerValue * 1.15 + 1; //add a bit to the timerValue
         timewait = timewait + 0.0005;
-        if (timer >= 30) timer = 30;
+        if (timerValue >= 30) timerValue = 30;
     } else {
-        score = 0;
-        alert("You have lost. Resetting.");
-        timewait = 0.01;
-        timer = 30;
 
+        alert(`You have lost with a score of : ${score}. Resetting.`);
+        score = 0;
+        timewait = 0.01;
+        timerValue = 30;
     }
     $("#score").text(`Score: ${score}`);
 };
 
-
-$(document).keyup(function (e) {
-    console.log(e.keyCode);
-    if (colorsc[e.keyCode] === undefined) return;
-    check(e.keyCode);
-    //randomize();
-});
 randomize();
 
-//hack();
-
-
-
 function hack() {
-    setInterval(function () {
+    setInterval(function() {
         if (subject.style.backgroundColor === top_b.style.backgroundColor) {
-            //  console.log("select up.");
             check(38);
         } else if (subject.style.backgroundColor === bottom.style.backgroundColor) {
-            //  console.log("select down.");
             check(40);
         }
-    }, ranTIMES[Math.floor(Math.random() * ranTIMES.length)]);
+    }, 1);
     //ranTIMES[Math.floor(Math.random() * ranTIMES.length)]
 }
 
-setInterval(function () {
-    var x = document.getElementsByClassName("timer");
-    timer -= timewait;
-    x[0].style.width = `${timer}%`;
-    x[1].style.width = `${timer}%`;
-    if (timer < 0) {
-        alert("You have lost. Resetting.");
-        timer = 30;
-        timewait = 0.01;
-        score = 0;
-        $("#score").text(`Score: ${score}`);
-    }
-}, 0.1);
+
+
+$(document).keyup(function(e) {
+    console.log(e.keyCode);
+    if (colorsc[e.keyCode] === undefined) return;
+    check(e.keyCode);
+});
+top_b.onclick = function() {
+    check(38);
+};
+bottom.onclick = function() {
+    check(40);
+};
+var started = false;
+var game;
+var butint = document.getElementById("gameres");
+function startgame() {
+  if(started == false){
+    butint.innerHTML = "Stop Game.";
+  game = setInterval(function() {
+        var x = document.getElementsByClassName("timer");
+        timerValue -= timewait;
+        x[0].style.width = `${timerValue}%`;
+        x[1].style.width = `${timerValue}%`;
+        if (timerValue < 0) {
+            alert(`You have lost with a score of : ${score}. Resetting.`);
+            timerValue = 30;
+            timewait = 0.01;
+            score = 0;
+            $("#score").text(`Score: ${score}`);
+        }
+    }, 0.1);
+    started = true;
+  } else {
+    clearInterval(game);
+    started = false;
+    butint.innerHTML = "Start Game.";
+  }
+}
