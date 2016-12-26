4e083d233e1fa05e6e3252a7f5849b18bad077c6
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-12-25 22:07:47 -0500

    Reorginized code.js

--- code.js
+++ code.js
@@ -1,17 +1,85 @@
+/**
+ * Made by Viktor Korolyuk.
+ * Copyright blahblahblah,... viktor add info here.
+ */
+
 var top_b = document.getElementById("top");
 var bottom = document.getElementById("bottom");
+var butint = document.getElementById("gameres");
+var scoreElem = document.getElementById("score");
 var subject = document.getElementById("subject");
+var score = 0;
+var timewait = 0.01;
+var timerValue = 30;
+var started = false;
+var gameLoop; //setting an empty variable to represent the gameloop lateron.
+var colors = [
+    "rgb(255, 237, 102)",
+    "rgb(186, 218, 85)",
+    "rgb(75, 204, 220)"
+];
 var colorsc = {
     38: "",
     40: ""
 };
-var score = 0;
-var colors = ["rgb(255, 237, 102)", "rgb(186, 218, 85)", "rgb(75, 204, 220)"];
-var ranTIMES = [2500, 1000, 2000, 5000, 3000, 1000, 5000];
-var timewait = 0.01;
-var timerValue = 30;
 
-var randomize = function() {
+
+function startgame() {
+    if (started == false) {
+        butint.innerHTML = "Stop Game.";
+        gameLoop = setInterval(function () {
+            var x = document.getElementsByClassName("timer");
+            timerValue -= timewait;
+            x[0].style.width = `${timerValue}%`;
+            x[1].style.width = `${timerValue}%`;
+            if (timerValue < 0) {
+                loose();
+            }
+        }, 0.1);
+        started = true;
+    } else {
+        loose();
+    }
+}
+//boolean logic checking if the key pressed is the one the game needs, but not the one is deserves.
+var check = function (key) {
+    if (colorsc[key] === subject.style.backgroundColor) {
+        score++;
+        randomize();
+        timerValue = timerValue * 1.15 + 1; //add a bit to the timerValue
+        timewait = timewait + 0.0005;
+        if (timerValue >= 30) timerValue = 30;
+        scoreElem.innerHTML = `Score: ${score}`;
+    } else {
+        loose();
+    }
+
+};
+//a function to stop code repetition
+var loose = function () {
+    alert(`You have lost with a score of : ${score}. Resetting.`);
+    score = 0;
+    timewait = 0.01;
+    timerValue = 30;
+    clearInterval(gameLoop);
+    started = false;
+    butint.innerHTML = "Start Game.";
+    scoreElem.innerHTML = `Score: ${score}`;
+};
+
+//a bot to run the game whilist I am too lazy.
+var hack = function () {
+    setInterval(function () {
+        if (subject.style.backgroundColor === top_b.style.backgroundColor) {
+            check(38);
+        } else if (subject.style.backgroundColor === bottom.style.backgroundColor) {
+            check(40);
+        }
+    }, 1);
+}
+
+//setting the randomize function up for later use.
+var randomize = function () {
     var wheight = colors[Math.floor(Math.random() * colors.length)];
     var wheight2 = colors[Math.floor(Math.random() * colors.length)];
     var opt = [];
@@ -27,72 +95,18 @@ var randomize = function() {
     subject.style.backgroundColor = opt[Math.floor(Math.random() * opt.length)];
 };
 
-var check = function(key) {
-    if (colorsc[key] === subject.style.backgroundColor) {
-        score++;
-        randomize();
-        timerValue = timerValue * 1.15 + 1; //add a bit to the timerValue
-        timewait = timewait + 0.0005;
-        if (timerValue >= 30) timerValue = 30;
-    } else {
 
-        alert(`You have lost with a score of : ${score}. Resetting.`);
-        score = 0;
-        timewait = 0.01;
-        timerValue = 30;
-    }
-    $("#score").text(`Score: ${score}`);
-};
-
-randomize();
-
-function hack() {
-    setInterval(function() {
-        if (subject.style.backgroundColor === top_b.style.backgroundColor) {
-            check(38);
-        } else if (subject.style.backgroundColor === bottom.style.backgroundColor) {
-            check(40);
-        }
-    }, 1);
-    //ranTIMES[Math.floor(Math.random() * ranTIMES.length)]
-}
-
-
-
-$(document).keyup(function(e) {
+//Setting up the listeners.
+document.onkeyup = function (e) {
     console.log(e.keyCode);
     if (colorsc[e.keyCode] === undefined) return;
     check(e.keyCode);
-});
-top_b.onclick = function() {
+};
+top_b.onclick = function () {
     check(38);
 };
-bottom.onclick = function() {
+bottom.onclick = function () {
     check(40);
 };
-var started = false;
-var game;
-var butint = document.getElementById("gameres");
-function startgame() {
-  if(started == false){
-    butint.innerHTML = "Stop Game.";
-  game = setInterval(function() {
-        var x = document.getElementsByClassName("timer");
-        timerValue -= timewait;
-        x[0].style.width = `${timerValue}%`;
-        x[1].style.width = `${timerValue}%`;
-        if (timerValue < 0) {
-            alert(`You have lost with a score of : ${score}. Resetting.`);
-            timerValue = 30;
-            timewait = 0.01;
-            score = 0;
-            $("#score").text(`Score: ${score}`);
-        }
-    }, 0.1);
-    started = true;
-  } else {
-    clearInterval(game);
-    started = false;
-    butint.innerHTML = "Start Game.";
-  }
-}
+//when this code is loaded, run randomize to make sure its always "random"... hehehe... small jokes are always funny.
+randomize();
