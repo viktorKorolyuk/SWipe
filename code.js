c774327c4fdc5dda39d0eb32596e0fccc0d78e22
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2017-07-17 01:12:22 -0400

    Nitpicking
    - Altered game CSS
    - Corrected comment grammar
    - Auto-formatted code

--- code.js
+++ code.js
@@ -1,59 +1,82 @@
-/**
- * Made by Viktor Korolyuk.
- * Copyright blahblahblah,... viktor add info here.
+/*
+ * Name: code.js
+ * Task: set up SWipe game
  */
-var gh = GameHandle;
+
+//Creating new object with prototype of GameHandle
+var gh = Object.create(GameHandle);
+
 var top_b = document.getElementById("top");
+
 var bottom = document.getElementById("bottom");
+
 var butint = document.getElementById("gameres");
+
 var scoreElem = document.getElementById("score");
+
 var subject = document.getElementById("subject");
+
 var score = 0;
+
 var timewait = 0.01;
+
 var timerValue = 30;
+
 var started = false;
-var gameLoop; //setting an empty variable to represent the gameloop lateron.
+
+ //setting an empty variable to represent the gameloop lateron.
+var gameLoop;
+
+// Array of all progress bars
+var timers = document.getElementsByClassName("timer");
 var colors = [
-    "rgb(255, 237, 102)",
-    "rgb(76, 173, 214)",
-    "rgb(245, 40, 55)"
+  "rgb(255, 237, 102)",
+  "rgb(76, 173, 214)",
+  "rgb(245, 40, 55)"
 ];
+
 var colorsc = {
-    38: "",
-    40: ""
+  38: "",
+  40: ""
 };
 
 function startgame() {
-    if (started == false) {
-        butint.innerHTML = "Stop Game.";
-        gameLoop = setInterval(function () {
-            var x = document.getElementsByClassName("timer");
-            timerValue -= timewait;
-            x[0].style.width = `${timerValue}%`;
-            x[1].style.width = `${timerValue}%`;
-            if (timerValue < 0) {
-                gh.loose();
-            }
-        }, 0.1);
-        started = true;
-    } else {
+  if (!started) {
+    butint.innerHTML = "Stop Game.";
+    gameLoop = setInterval(function() {
+      timerValue -= timewait;
+      timers[0].style.width = `${timerValue}%`;
+      timers[1].style.width = `${timerValue}%`;
+      if (timerValue < 0) {
         gh.loose();
-    }
+      }
+    }, 0.1);
+    started = true;
+  } else {
+    // If the user stops early, they lost
+    gh.loose();
+  }
 }
 
-//Setting up the listeners.
-document.onkeyup = function (e) {
-    console.log(e.keyCode);
-    if (colorsc[e.keyCode] === undefined) return;
-    gh.check(e.keyCode);
+// Setting up the listeners.
+document.onkeyup = function(e) {
+
+  // Check if the keycode is supported
+  if (!colorsc[e.keyCode]) return;
+  if(!started){
+    startgame();
+  }
+  gh.check(e.keyCode);
 };
-top_b.onclick = function () {
-    if (!started) startgame();
-    gh.check(38);
+top_b.onclick = function() {
+  if (!started) startgame();
+  gh.check(38);
 };
-bottom.onclick = function () {
-    if (!started) startgame();
-    gh.check(40);
+bottom.onclick = function() {
+  if (!started) startgame();
+  gh.check(40);
 };
-//when this code is loaded, run randomize to make sure its always "random"... hehehe... small jokes are always funny.
+
+// When this code is loaded, run randomize to make sure its always "random"... hehehe... small jokes are always funny.
+// EDIT: 2017-06-15; I don't get the joke. How was that funny?
 gh.randomize();
--- gamehandle.js
+++ gamehandle.js
@@ -16,7 +16,8 @@ GameHandle.check = function (key) {
     if (colorsc[key] === subject.style.backgroundColor) {
         score++;
         this.randomize();
-        timerValue = timerValue * 1.15 + 1; //add a bit to the timerValue
+        // Add a bit to the timerValue
+        timerValue = timerValue * 1.15 + 1;
         timewait = timewait + 0.0005;
         if (timerValue >= 30) timerValue = 30;
         scoreElem.innerHTML = `Score: ${score}`;
@@ -25,8 +26,8 @@ GameHandle.check = function (key) {
     }
 }
 
-//setting the randomize function up for later use.
-//The methods do not require inputs because the variables are global in code.js.
+// Setting the randomize function up for later use.
+// The methods do not require inputs because the variables are global in code.js.
 GameHandle.randomize = function () {
     var wheight = colors[Math.floor(Math.random() * colors.length)];
     var wheight2 = colors[Math.floor(Math.random() * colors.length)];
@@ -34,7 +35,7 @@ GameHandle.randomize = function () {
     opt.push(wheight);
     opt.push(wheight2);
     while (wheight2 === wheight) {
-        wheight2 = colors[Math.floor(Math.random() * colors.length)]; //make sure the value is never the same as the first one.
+        wheight2 = colors[Math.floor(Math.random() * colors.length)]; // Make sure the value is never the same as the first one.
     }
     colorsc[38] = wheight;
     colorsc[40] = wheight2;
@@ -43,8 +44,8 @@ GameHandle.randomize = function () {
     subject.style.backgroundColor = opt[Math.floor(Math.random() * opt.length)];
 }
 
-//a bot to run the game whilist I am too lazy to play.
-GameHandle.hack = function () {
+// A bot to run the game whilist I am too lazy to play.
+GameHandle.bot = function () {
     setInterval(function () {
         if (subject.style.backgroundColor === top_b.style.backgroundColor) {
             gh.check(38);
